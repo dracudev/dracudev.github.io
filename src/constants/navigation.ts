e62065d095be9430aefb414bdd65fdbcf0c13f30
dracudev <andreujavier99@gmail.com>
2025-05-10 13:17:11 +0200

    Add navigation component and constants for main navigation links

---
+++ src/constants/navigation.ts
@@ -0,0 +1,20 @@
+import type { Navigation } from "../types/navigation";
+
+export const NAVIGATION: Navigation = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "Projects",
+    path: "/projects",
+  },
+  {
+    name: "Experience",
+    path: "/experience",
+  },
+  {
+    name: "Education",
+    path: "/education",
+  },
+];

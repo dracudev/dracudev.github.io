f01a5b06d5df5f9b37f6e7a6dc5c4e373faf2ab2
dracudev <andreujavier99@gmail.com>
2025-05-24 16:49:11 +0200

    Add type definitions for projects and props, and utility types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,18 +1,11 @@
 export interface SocialLink {
-    platform: string;
-    url: string;
+  platform: string;
+  url: string;
 }
 
 export interface MetaData {
-    title: string;
-    description: string;
-    image?: string;
-    canonicalURL?: string;
+  title: string;
+  description: string;
+  image?: string;
+  canonicalURL?: string;
 }
-
-export interface ContentFrontmatter {
-    title: string;
-    description: string;
-    publishedAt: Date;
-    draft?: boolean;
-} 
